37489f26ea4fee3065940d12a1f805f921bfbf7b
AlvaroGP <customerm552@gmail.com>
2024-01-11 22:08:57 -0500

    Ejemplos y ordenar carpetas

---
+++ Conceptos/@classmethod_@staticmethod.py
@@ -0,0 +1,74 @@
+'''Decoradores:
+ @staticmethod @classmethod'''
+
+class Pintar():
+    def __init__(self, color) -> None:
+        self.color = color
+#Atributo de clase
+#Pertenece a la clase y a todas sus instancias
+    superficie = 'Pared'    
+    
+    @classmethod
+    def metodo_clase(cls):
+        #Se implementa para acceder a los atributos de clase
+        #toda modificacion en la clase afecta a todas las 
+        #instancias
+        #Se llama desde la clase
+        #No puede acceder a los atributos de instancia
+        print('Este es un metodo de clase...')
+        print('Desde aqui modificar la superficie') 
+   
+    @staticmethod
+    def metodo_estatico():
+        #Es una funcion que depende siempre de la clase, no se implementa en las instancias
+        # No tiene acceso a los atributos ni de la clase ni de la instancia
+        #Se llama desde la clase
+        #Se usa cuando quiero usar un metodo sin necesidad de instanciar un objeto
+        print('Este es un metodo estatico...')
+
+    def metodo_instancia(self):
+        # El normal que se modifica en las instancias sin afectar a las otras ni a la clase
+        # Se llama desde la instancia
+        print('Este es un metodo de instancia...')
+
+#Ejemplo @classmethod
+
+class Figura():
+    def __init__(self,color) -> None:
+        self.color = color
+
+    forma = "Tiangulo"
+    
+    #Se puede modificar un atributo de clase
+    #desde un metodo de clase
+    @classmethod
+    def cambiar_forma(cls,nueva_forma):
+        print('Implementar alguna logica')
+        cls.forma = nueva_forma
+
+figura1 = Figura('amarillo')
+figura2 = Figura('azul')
+figura3 = Figura('rojo')
+
+print(figura1.forma)
+print(figura1.color)
+#Los cambios en los atributos de clase
+#que se hagan por asignacion directa al atributo
+#de clase o mediante el @classmethod se veran
+#reflejados en todas las instancias.
+Figura.cambiar_forma("Cuadrado")
+print(figura1.forma)
+print(figura1.color)
+print(figura2.color)
+print(figura2.forma)
+print(Pintar.metodo_estatico())
+
+
+
+
+
+
+
+    
+
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ProgramacionOrientadaObjetosPython
+ProgramacionOrientadaObjetosPython
 Desarrollo de los principales conceptos
 de la Programacion Orientada a Objetos 
-en Python
+en Python.
